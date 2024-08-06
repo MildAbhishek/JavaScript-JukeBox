8a23b77d639b28b75055c996cc8a2c502d33680f
Abhishek Anand <64414279+MildAbhishek@users.noreply.github.com>
2024-08-06 18:04:51 +0000

    Date In JS

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,52 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString()); // Tue Aug 06 2024 17:47:52 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Tue Aug 06 2024
+// console.log(myDate.toLocaleString()); // 8/6/2024, 5:47:52 PM
+// console.log(typeof myDate); // object
+
+//let myCreatedDate = new Date(2023, 0, 23) // 1/23/2023, 12:00:00 AM
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3) // /23/2023, 5:03:00 AM
+// let myCreatedDate = new Date("2023-01-14") // 1/14/2023, 12:00:00 AM
+
+let myCreatedDate = new Date("01-14-2023")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()}
+
+// Customizing the date
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
+// Date : Date is an object in javascript. 
+// JavaScript stores dates as number of milliseconds since January 01, 1970
+/*
+const myDate = new Date();
+console.log(myDate) //Output : 2023-10-09T14:38:09.847Z
+// We can use various methods to convert this date into more readable value.
+console.log(myDate.toString()) //Output : Mon Oct 09 2023 07:39:18 GMT-0700 (Pacific Daylight Time)
+
+console.log(myDate.toLocaleString()) // Output : 10/9/2023, 7:44:20 AM
+console.log(myDate.toLocaleDateString()) //Output : 10/9/2023
+console.log(myDate.toLocaleTimeString()) // Output : 7:45:31 AM
+
+console.log(myDate.toDateString()) //Output : Mon Oct 09 2023
+console.log(myDate.toTimeString()) // Output : 07:46:40 GMT-0700 (Pacific Daylight Time)
+
+console.log(myDate.toISOString()) // Output : 2023-10-09T14:43:39.337Z
+console.log(myDate.toJSON()) //Output : 2023-10-09T14:40:58.495Z
+*/
